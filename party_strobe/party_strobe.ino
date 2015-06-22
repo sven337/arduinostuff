adcf081553f9448e61bb8f28cfd32a234784f572
Sven337 <e5d0ceda@opayq.com>
2015-06-22 22:36:51 +0200

    make strobe and ramp work again

--- party_strobe/party_strobe.ino
+++ party_strobe/party_strobe.ino
@@ -35,9 +35,13 @@ typedef signed long fixedpoint;
 #define I2F(I) (I << 20)
 #define F2I(F) (F >> 20)
 
-static fixedpoint led_r = 0;
-static fixedpoint led_g = 0;
-static fixedpoint led_b = 0;
+static uint8_t led_r = 0;
+static uint8_t led_g = 0;
+static uint8_t led_b = 0;
+
+static fixedpoint led_r_fp;
+static fixedpoint led_g_fp;
+static fixedpoint led_b_fp;
 
 static fixedpoint delta_r;
 static fixedpoint delta_g;
@@ -45,6 +49,8 @@ static fixedpoint delta_b;
 
 static unsigned long ramp_start_time;
 static unsigned long ramp_last_step;
+
+bool change_mode_button_pressed;
 
 enum mode {
 	OFF = 0, 
@@ -81,23 +87,18 @@ const struct ramp LED_ramps[] = {
         [FADE7_END] = { dur,   0.7,   0.7,     0.7, FADE7 }, //FADE7_END
 };
 
+// Average magnitudes for frequency bins
+uint8_t freq_avg_mag[3];
 
 void on_off_interrupt(void)
 {
 	// Potentiometer on/off enables/disables strobe from music mode
 	strobe_off = PCintPort::pinState;
-	printf("strobe_off: %d\n", strobe_off);
 }
 
 void button_interrupt(void)
 {
-	if (current_mode == STROBE) {
-		current_mode = OFF;
-	} else {
-		current_mode = (enum mode)(current_mode + 1);
-	}
-
-	printf("current mode is %d\n", current_mode);
+	change_mode_button_pressed = true;
 }
 
 uint8_t bri(float in)
@@ -108,14 +109,14 @@ uint8_t bri(float in)
 //	return (uint8_t)(255.0 * in);
 }
 
-void set_led(long r, long g, long b) //fixedpoint!
+void set_led(uint8_t r, uint8_t g, uint8_t b)
 {
 	led_r = r;
 	led_g = g;
 	led_b = b;
 
 	uint8_t my_r, my_g, my_b;
-#define BRI_CORRECT(X) (bri(F2I(X)) & 0xFF)
+#define BRI_CORRECT(X) (bri(X) & 0xFF)
 	my_r = BRI_CORRECT(r);
 	my_g = BRI_CORRECT(g);
 	my_b = BRI_CORRECT(b);
@@ -134,9 +135,13 @@ void start_ramp(int which)
 	ramp_last_step = ramp_start_time;
 
 	// Compute per-millisecond deltas
-	delta_r = (I2F((int32_t)(255.0 * r->r)) - led_r) / r->duration;
-	delta_g = (I2F((int32_t)(255.0 * r->g)) - led_g) / r->duration;
-	delta_b = (I2F((int32_t)(255.0 * r->b)) - led_b) / r->duration;
+
+	led_r_fp = I2F((int32_t)led_r);
+	led_g_fp = I2F((int32_t)led_g);
+	led_b_fp = I2F((int32_t)led_b);
+	delta_r = (I2F((int32_t)(255.0 * r->r)) - led_r_fp) / r->duration;
+	delta_g = (I2F((int32_t)(255.0 * r->g)) - led_g_fp) / r->duration;
+	delta_b = (I2F((int32_t)(255.0 * r->b)) - led_b_fp) / r->duration;
 
 	printf("Start ramp %d deltas r %ld g %ld b %ld\n", doing_ramp, delta_r, delta_g, delta_b);
 	current_mode = RAMP;
@@ -154,7 +159,7 @@ void led_ramp(void)
 	if (delay_in_ramp > r->duration) {
 		// Ramp is done, move on
 		if (r->next_ramp != doing_ramp) {
-			set_led(I2F((uint32_t)(255.0 * r->r)), I2F((uint32_t)(255.0 * r->g)), I2F((uint32_t)(255.0 * r->b)));
+			set_led((uint8_t)(255.0 * r->r), (uint8_t)(255.0 * r->g), (uint8_t)(255.0 * r->b));
 			start_ramp(r->next_ramp);
 		}
 		return;
@@ -163,13 +168,18 @@ void led_ramp(void)
 	unsigned long this_step_duration = now - ramp_last_step;
 	ramp_last_step = now;
 
-	set_led(led_r + this_step_duration * delta_r, led_g + this_step_duration * delta_g, led_b + this_step_duration * delta_b);
+	led_r_fp = led_r_fp + this_step_duration * delta_r;
+	led_g_fp = led_g_fp + this_step_duration * delta_g;
+	led_b_fp = led_b_fp + this_step_duration * delta_b;
+
+	printf("ramp r %ld g %ld b %ld\n", led_r_fp, led_g_fp, led_b_fp);
+	set_led(F2I(led_r_fp), F2I(led_g_fp), F2I(led_b_fp));
 }
 
 void strobe()
 {
 	static int last_wait = 0;
-	static fixedpoint next = 0;
+	static uint8_t next = 0;
 	static long next_at = 0;
 
 	long now = millis();
@@ -178,15 +188,13 @@ void strobe()
 	float freq = 1 + sequence_speed_factor * 30;
 	wait = 1000.0 / freq;
 	if (wait != last_wait) {
-		printf("Wait = %d\n", wait);
 		last_wait = wait;
 	}
 
 	if (now > next_at) {
 		set_led(next, next, next);
 		next_at = now + wait;
-		next = I2F((uint32_t)255) - next;
-		printf("next = %d\n", next);
+		next = 255 - next;
 	} else {
 		if (next_at - now > wait)
 			next_at = now + wait;
@@ -195,43 +203,104 @@ void strobe()
 
 void music()
 {
-int chnLow = 3;
-int chnMid = 4;
-int chnHigh = 5;
+	Serial.print("music");
+	const int oct_low = 3;
+	const int oct_mid = 4;
+	const int oct_high = 5;
 
+	int timer = TIMSK0;
+	int adcsra = ADCSRA;
+	ADCSRA = 0xe5; // set the adc to free running mode
+	TIMSK0 = 0;
 	// FFT and set leds accordingly
-		cli();  // UDRE interrupt slows this way down on arduino1.0
-		for (int i = 0 ; i < FHT_N ; i++) { // save 256 samples
-			while(!(ADCSRA & 0x10)); // wait for adc to be ready
-			ADCSRA = 0xf5; // restart adc
-			byte m = ADCL; // fetch adc data
-			byte j = ADCH;
-			int k = (j << 8) | m; // form into an int
-			k -= 0x0200; // form into a signed int
-			k <<= 6; // form into a 16b signed int
-			fht_input[i] = k; // put real data into bins
-		}
-		fht_window(); // window the data for better frequency response
-		fht_reorder(); // reorder the data before doing the fht
-		fht_run(); // process the data in the fht
-		fht_mag_octave(); // take the output of the fht
-		sei();
-/*		Serial.write(255); // send a start byte
-		Serial.write(fht_log_out, FHT_N/2); // send out the data*/
-//		printf("got %d %d %d\n", fht_log_out[chnLow], fht_log_out[chnMid], fht_log_out[chnHigh]);
-	/*	for (int i = 0; i < LOG_N; i++) {
-			printf("Bin %d value %d\n", i, fht_log_out[i]);
-		}*/
-    fht_oct_out[chnLow] = map(constrain(fht_oct_out[chnLow]-89, 0, 250), 0, 200, 0, 150); //low
-    fht_oct_out[chnMid] = map(constrain(fht_oct_out[chnMid]-25, 0, 250), 0, 240, 0, 150); //mid
-    fht_oct_out[chnHigh] = map(constrain(fht_oct_out[chnHigh]-15, 0, 250), 0, 240, 0, 150); //high*/
+	cli();  // UDRE interrupt slows this way down on arduino1.0
+	for (int i = 0 ; i < FHT_N ; i++) { // save 256 samples
+		while(!(ADCSRA & 0x10)); // wait for adc to be ready
+		ADCSRA = 0xf5; // restart adc
+		byte m = ADCL; // fetch adc data
+		byte j = ADCH;
+		int k = (j << 8) | m; // form into an int
+		k -= 0x0200; // form into a signed int
+		k <<= 6; // form into a 16b signed int
+		fht_input[i] = k; // put real data into bins
+	}
+	fht_window(); // window the data for better frequency response
+	fht_reorder(); // reorder the data before doing the fht
+	fht_run(); // process the data in the fht
+	fht_mag_octave(); // take the output of the fht
+	sei();
+	TIMSK0 = timer;
+	ADCSRA = adcsra;
+		
+	enum operation { ERROR, RISE, SLOW_DECAY, FAST_DECAY };
+
+	struct {
+		int magidx;
+		int bias;
+		// int avgidx is equal to i;
+		enum operation op;
+	} channels[] = {
+			{ oct_low , 0, ERROR },
+			{ oct_mid , 0, ERROR },
+			{ oct_high, 0, ERROR },
+	};
+
+	for (int i = 0; i < 3; i++) {
+		int bias = channels[i].bias;
+		int magidx = channels[i].magidx;
+
+		// Remove constant component and remap to 0..255	
+
+		fht_oct_out[magidx] = map(fht_oct_out[magidx] - bias, 0, 255-bias, 0, 255);
 	
-	//with bias
-   
-   printf("%d %d %d\n", fht_oct_out[chnLow], fht_oct_out[chnMid], fht_oct_out[chnHigh]);
-	set_led(I2F((uint32_t)fht_oct_out[chnHigh]), 
-			I2F((uint32_t)fht_oct_out[chnMid]), 
-			I2F((uint32_t)fht_oct_out[chnLow]));
+		// Compare to moving average and make a decision based on that.
+		// 		value < avg: decay (faster as value is lower)
+		// 		value = avg: slow decay
+		// 		value > avg: rise exponentially
+		if (abs(fht_oct_out[magidx] - freq_avg_mag[i]) < 7) {
+			// Roughly equal, so slow decay
+			channels[i].op = SLOW_DECAY;
+		} else if (fht_oct_out[magidx] < freq_avg_mag[i]) {
+			channels[i].op = FAST_DECAY;
+		} else {
+			channels[i].op = RISE;
+		}
+
+		// Update moving average
+		freq_avg_mag[i] = (3 * freq_avg_mag[i] + fht_oct_out[magidx]) / 4;
+	}
+
+	int new_led[3];
+	for (int i = 0; i < 3; i++) {
+		new_led[i] = (i == 0) ? led_b : ((i == 1) ? led_g : led_r);
+		switch (channels[i].op) {
+			case SLOW_DECAY:
+				if (new_led[i] < 5) {
+					new_led[i] = 0;
+				} else {
+					new_led[i] -= 5;
+				}
+				break;
+			case FAST_DECAY:
+				if (new_led[i] < 10) {
+					new_led[i] = 0;
+				} else {
+					new_led[i] -= 10;
+				}
+				break;
+			case RISE:
+				if (new_led[i] > 200) {
+					new_led[i] = 255;
+				} else {
+					new_led[i] += 50;
+				}
+				break;
+		}
+	}	
+
+   printf("%d %d %d (avg %d %d %d)->r %d g %d b %d\n", fht_oct_out[oct_low], fht_oct_out[oct_mid], fht_oct_out[oct_high], freq_avg_mag[0], freq_avg_mag[1], freq_avg_mag[2], new_led[2], new_led[1], new_led[0]);
+
+   set_led(new_led[2], new_led[1], new_led[0]);
 
 
 }
@@ -258,13 +327,11 @@ void setup()
 
 	strobe_off = digitalRead(ON_OFF_PIN);
 
-	current_mode = MUSIC;
+	current_mode = STROBE;
 //	start_ramp(FADE7);
-
-	TIMSK0 = 0; // turn off timer0 for lower jitter
-	ADCSRA = 0xe5; // set the adc to free running mode
 	ADMUX = 0x40; // use adc0
 	DIDR0 = 0x01; // turn off the digital input for adc0
+
 }
 
 void loop()
@@ -277,6 +344,19 @@ void loop()
 	   val = constrain(val, 0.0, 1.0);
 	   sequence_speed_factor = val;
 	}	   
+
+	if (change_mode_button_pressed) {
+		if (current_mode == STROBE) {
+			current_mode = OFF;
+		} else {
+			current_mode = (enum mode)(current_mode + 1);
+			if (current_mode == RAMP) {
+				start_ramp(FADE7);
+			}
+		}
+		change_mode_button_pressed = false;
+		printf("current mode is %d\n", current_mode);
+	}
 
 
 	switch (current_mode) {

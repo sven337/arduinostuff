60426aa3a255bf7d45b91091ac3dc71d02b9f44d
Sven337 <e5d0ceda@opayq.com>
2016-04-26 17:33:20 +0200

    babymonitor recv: first attempt with mcp4725 DAC

--- babymonitor/recv/recv.ino
+++ babymonitor/recv/recv.ino
@@ -1,17 +1,21 @@
+#include <Wire.h>
 #define USE_US_TIMER
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiUdp.h>
-#include <ESP8266WebServer.h>
 #define UPDATER_NO_MDNS
 #include "ESP8266mDNS.h"
 #include <ArduinoOTA.h>
-#include <Ticker.h>
 
 #include "wifi_params.h"
+#define ICACHE_RODATA_ATTR  __attribute__((section(".irom.text")))
+
 #include "sounddata.h"
 
-const int snd = 12;
+const int mySDA = 4;
+const int mySCL = 5;
+
+TwoWire i2c;
 
 uint32_t usToTicks(uint32_t us)
 {
@@ -20,27 +24,41 @@ uint32_t usToTicks(uint32_t us)
 
 void playsample_isr(void)
 {
+	/*static uint16_t value = 0;
+	DAC(value++);
+	if (value == (1 << 12)) {
+		value = 0;
+	}*/
 	static int idx = 0;
-	analogWrite(snd, sounddata_data[idx++]);
+	DAC(sounddata_data[idx++]);
 	if (idx > sizeof(sounddata_data)/sizeof(sounddata_data[0])) {
 		idx = 0;
 	}
+	
 }
+
+void DAC(uint16_t value) 
+{
+	i2c.beginTransmission(0x60); // address is 0x60
+	i2c.write(0x40); // command is 0x40
+	i2c.write((value >> 4) & 0xFF);
+	i2c.write((value & 0x0F) << 4);
+	i2c.endTransmission();
+}
+
 void setup ( void ) {
-	Serial.begin ( 57600 );
+	Serial.begin ( 74880 );
 	Serial.println("setup");
-	pinMode (snd, OUTPUT);
-	analogWriteFreq(50000);
-	analogWriteRange(256);
+	i2c.begin(mySDA, mySCL);
+	i2c.setClock(400000);
 
 	Serial.println("done setup");
 
 	timer1_isr_init();
 	timer1_attachInterrupt(playsample_isr);
-	timer1_enable(TIM_DIV16, TIM_EDGE, TIM_SINGLE);
-	timer1_write(usToTicks(1000000/8000));
+	timer1_enable(TIM_DIV16, TIM_EDGE, TIM_LOOP);
+	timer1_write(clockCyclesPerMicrosecond() / 16 * 80);
 }
 
 void loop ( void ) {
-	;
 }

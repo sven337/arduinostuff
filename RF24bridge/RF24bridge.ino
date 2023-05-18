cc71f4265ed31e599fac993e04f6543781e68dc4
Sven337 <e5d0ceda@opayq.com>
2023-05-18 15:39:14 +0200

    RF24bridge: kill teleinfo and ledlamp (not used any more)

--- RF24bridge/RF24bridge.ino
+++ RF24bridge/RF24bridge.ino
@@ -12,12 +12,10 @@ const int LED_RED = 4;
 const int DS18B20_PIN = 7;
 
 const uint64_t pipe_gaz 	 = 0xF0F0F0F0F0LL;
-const uint64_t pipe_ledlamp  = 0xF0F0F0F0F2LL;
 const uint64_t pipe_mailbox  = 0xF0F0F0F0F3LL;
 const uint64_t pipe_thermometer  = 0xF0F0F0F0F4LL;
 
 #define PIPE_GAZ_ID 1
-#define PIPE_LEDLAMP_ID 2
 #define PIPE_MAILBOX_ID 3
 #define PIPE_THERMOMETER_ID 4
 
@@ -32,16 +30,16 @@ int teleinfo_cur = 0;
 
 OneWire ds(DS18B20_PIN);
 uint32_t send_next_temperature_at = 1000;
-const uint8_t thermometer_identification_letter = 'P'; // "pantry"
+const uint8_t thermometer_identification_letter = 'R'; // "rochelle"
 
 struct {
     uint8_t addr[8];
     uint8_t letter;
 } thermometer_letter_from_addr[] = {
-        {{ 0x28, 0xff, 0xbd, 0xd3, 0x90, 0x15, 0x03, 0xbb }, 'P'},
-        {{ 0x28, 0x21, 0x5e, 0x79, 0xa2, 0x00, 0x03, 0xc8 }, 'B'},
-        {{ 0x28, 0x05, 0x46, 0x79, 0xa2, 0x00, 0x03, 0xe0 }, 'L'},
-        {{ 0x28, 0xd0, 0x41, 0x79, 0xa2, 0x00, 0x03, 0x9e }, 'K'},
+        {{ 0x28, 0xff, 0xbd, 0xd3, 0x90, 0x15, 0x03, 0xbb }, 'R'}, //rochelle (self)
+        {{ 0x28, 0x21, 0x5e, 0x79, 0xa2, 0x00, 0x03, 0xc8 }, 'B'}, //bedroom
+        {{ 0x28, 0x05, 0x46, 0x79, 0xa2, 0x00, 0x03, 0xe0 }, 'L'}, //living
+        {{ 0x28, 0xd0, 0x41, 0x79, 0xa2, 0x00, 0x03, 0x9e }, 'K'}, //kid
 };
 
 char serial_cmd[255];
@@ -124,14 +122,13 @@ int send_rf24_cmd(uint64_t addr, uint8_t param0, uint8_t param1, uint8_t param2,
 void setup()
 {
     printf_begin();
-    Serial.begin(57600);
-    printf("Hello\n");
+    Serial.begin(115200);
     edfSerial.begin(1200); 
 #if HAS_RF24
 	rf24.begin();
 	rf24.powerDown();
 	rf24.setRetries(15, 15);
-	rf24.setChannel(95);
+	rf24.setChannel(80);
 	rf24.setCRCLength(RF24_CRC_16);
 	rf24.setPayloadSize(sizeof(unsigned long));
 	rf24.setPALevel(RF24_PA_MAX);
@@ -139,7 +136,6 @@ void setup()
  	rf24.setAutoAck(true);
 
     rf24.openReadingPipe(PIPE_GAZ_ID, pipe_gaz);
-    rf24.openReadingPipe(PIPE_LEDLAMP_ID, pipe_ledlamp);
     rf24.openReadingPipe(PIPE_MAILBOX_ID, pipe_mailbox);
     rf24.openReadingPipe(PIPE_THERMOMETER_ID, pipe_thermometer);
 	rf24.startListening();
@@ -171,12 +167,25 @@ void setup()
 
 void consume_rf24()
 {
-    uint8_t data[4];
+    uint8_t p[4];
 	uint8_t pipe = 1;
 
 	 while (rf24.available(&pipe)) {
-		rf24.read(data, 4);
-        printf("RF24 p%d 0x%x 0x%x 0x%x 0x%x\n", pipe, data[0], data[1], data[2], data[3]);
+		rf24.read(p, 4);
+        bool decoded = true;
+        /*
+        if (p[0] == 'B') {
+            uint16_t value = p[2] << 8 | p[3];
+            float volt = value*3.3f/1024; //no voltage divider so no 2*
+            float level = (volt-0.8)/(1.5-0.8); //boost cutoff at 0.8
+            printf("Thermometer %c battery level: %ddV = %d%%\r\n", p[1], (int)(volt * 10.0), (int)(100.0*level));
+        } else if (p[0] == 'T') {
+            int16_t temperature = p[2] << 8 | p[3];
+            printf("Thermometer %c temperature: %d\r\n", p[1], temperature * 10 / 16);
+        }*/
+        
+        printf("RF24 p%d 0x%x 0x%x 0x%x 0x%x\r\n", pipe, p[0], p[1], p[2], p[3]);
+
     }
 }
 
@@ -184,25 +193,7 @@ void serial_command(char *cmd)
 {
     printf("Command \"%s\"\n", cmd);
 #define MATCHSTR(BUF,STR) !strncasecmp(BUF, STR, strlen(STR))
-    if (MATCHSTR(cmd, "LEDLAMP ")) {
-        char *p = cmd + strlen("LEDLAMP ");
-        int val = atoi(p);
-        int retry = 1;
-
-        if (MATCHSTR(p, "query")) {
-            while (retry-- && send_rf24_cmd(pipe_ledlamp, 'Q', 0, 0, 0)) {
-                delay(30);
-            }
-        } else if (MATCHSTR(p, "fade")) {
-            while (retry-- && send_rf24_cmd(pipe_ledlamp, 'F', 0, 0, 0)) {
-                delay(30);
-            }
-        } else {
-            while (retry-- && send_rf24_cmd(pipe_ledlamp, 'L', val, 0, 0)) {
-                delay(30);
-            }
-        }
-    } else if (MATCHSTR(cmd, "RADIO")) {
+    if (MATCHSTR(cmd, "RADIO")) {
         rf24.printDetails();
     } else if (MATCHSTR(cmd, "PING")) {
         printf("PONG\n");
@@ -284,9 +275,9 @@ void send_temperature()
 
 void loop()
 {
-    if (edfSerial.available()) {
+/*    if (edfSerial.available()) {
         consume_teleinfo();
-    }
+    }*/
 
     while (Serial.available()) {
         char c = Serial.read();
